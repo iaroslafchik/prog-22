e40040f5dbd2b5ae4c854c750830ec2ade78c499
Yorick <y.kovanev@mail.ru>
2024-04-15 10:28:13 +0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace lab_2
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_0 = new System.Windows.Forms.Button();
+            this.button_3 = new System.Windows.Forms.Button();
+            this.button_2 = new System.Windows.Forms.Button();
+            this.button_1 = new System.Windows.Forms.Button();
+            this.button_6 = new System.Windows.Forms.Button();
+            this.button_5 = new System.Windows.Forms.Button();
+            this.button_4 = new System.Windows.Forms.Button();
+            this.button_9 = new System.Windows.Forms.Button();
+            this.button_8 = new System.Windows.Forms.Button();
+            this.button_7 = new System.Windows.Forms.Button();
+            this.button_ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(228, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.WordWrap = false;
+            // 
+            // button_0
+            // 
+            this.button_0.Location = new System.Drawing.Point(12, 298);
+            this.button_0.Name = "button_0";
+            this.button_0.Size = new System.Drawing.Size(35, 25);
+            this.button_0.TabIndex = 32;
+            this.button_0.Tag = "";
+            this.button_0.Text = "0";
+            this.button_0.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_3
+            // 
+            this.button_3.Location = new System.Drawing.Point(94, 267);
+            this.button_3.Name = "button_3";
+            this.button_3.Size = new System.Drawing.Size(35, 25);
+            this.button_3.TabIndex = 30;
+            this.button_3.Tag = "";
+            this.button_3.Text = "3";
+            this.button_3.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_2
+            // 
+            this.button_2.Location = new System.Drawing.Point(53, 267);
+            this.button_2.Name = "button_2";
+            this.button_2.Size = new System.Drawing.Size(35, 25);
+            this.button_2.TabIndex = 29;
+            this.button_2.Tag = "";
+            this.button_2.Text = "2";
+            this.button_2.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_1
+            // 
+            this.button_1.Location = new System.Drawing.Point(12, 267);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(35, 25);
+            this.button_1.TabIndex = 28;
+            this.button_1.Tag = "";
+            this.button_1.Text = "1";
+            this.button_1.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_6
+            // 
+            this.button_6.Location = new System.Drawing.Point(94, 236);
+            this.button_6.Name = "button_6";
+            this.button_6.Size = new System.Drawing.Size(35, 25);
+            this.button_6.TabIndex = 27;
+            this.button_6.Tag = "";
+            this.button_6.Text = "6";
+            this.button_6.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_5
+            // 
+            this.button_5.Location = new System.Drawing.Point(53, 236);
+            this.button_5.Name = "button_5";
+            this.button_5.Size = new System.Drawing.Size(35, 25);
+            this.button_5.TabIndex = 26;
+            this.button_5.Tag = "";
+            this.button_5.Text = "5";
+            this.button_5.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_4
+            // 
+            this.button_4.Location = new System.Drawing.Point(12, 236);
+            this.button_4.Name = "button_4";
+            this.button_4.Size = new System.Drawing.Size(35, 25);
+            this.button_4.TabIndex = 25;
+            this.button_4.Tag = "";
+            this.button_4.Text = "4";
+            this.button_4.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_9
+            // 
+            this.button_9.Location = new System.Drawing.Point(94, 205);
+            this.button_9.Name = "button_9";
+            this.button_9.Size = new System.Drawing.Size(35, 25);
+            this.button_9.TabIndex = 24;
+            this.button_9.Tag = "";
+            this.button_9.Text = "9";
+            this.button_9.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_8
+            // 
+            this.button_8.Location = new System.Drawing.Point(53, 205);
+            this.button_8.Name = "button_8";
+            this.button_8.Size = new System.Drawing.Size(35, 25);
+            this.button_8.TabIndex = 23;
+            this.button_8.Tag = "";
+            this.button_8.Text = "8";
+            this.button_8.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_7
+            // 
+            this.button_7.Location = new System.Drawing.Point(12, 205);
+            this.button_7.Name = "button_7";
+            this.button_7.Size = new System.Drawing.Size(35, 25);
+            this.button_7.TabIndex = 22;
+            this.button_7.Tag = "";
+            this.button_7.Text = "7";
+            this.button_7.Click += new System.EventHandler(this.AddDigit);
+            // 
+            // button_
+            // 
+            this.button_.Location = new System.Drawing.Point(53, 298);
+            this.button_.Name = "button_";
+            this.button_.Size = new System.Drawing.Size(35, 25);
+            this.button_.TabIndex = 33;
+            this.button_.Tag = "";
+            this.button_.Text = "2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(252, 335);
+            this.Controls.Add(this.button_);
+            this.Controls.Add(this.button_0);
+            this.Controls.Add(this.button_3);
+            this.Controls.Add(this.button_2);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.button_6);
+            this.Controls.Add(this.button_5);
+            this.Controls.Add(this.button_4);
+            this.Controls.Add(this.button_9);
+            this.Controls.Add(this.button_8);
+            this.Controls.Add(this.button_7);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_0;
+        private System.Windows.Forms.Button button_3;
+        private System.Windows.Forms.Button button_2;
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_6;
+        private System.Windows.Forms.Button button_5;
+        private System.Windows.Forms.Button button_4;
+        private System.Windows.Forms.Button button_9;
+        private System.Windows.Forms.Button button_8;
+        private System.Windows.Forms.Button button_7;
+        private System.Windows.Forms.Button button_;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_2
+{
+    public partial class Form1 : Form
+    {
+        double input = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            updateInputBox();
+        }
+
+        private void AddDigit(object sender, EventArgs e)
+        {
+            addDigit(int.Parse((sender as Button).Text));
+        }
+
+        void updateInputBox()
+        {
+            textBox1.Clear();
+            textBox1.AppendText(string.Format("{0}", input));
+            Refresh();
+        }
+
+        void addDigit(int digit)
+        {
+            input *= 10;
+            input += digit;
+            updateInputBox();
+        }
+    }
+}
